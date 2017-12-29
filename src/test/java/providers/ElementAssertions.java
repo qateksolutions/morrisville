220fe7923498163bc9f5461db8971a8838592c33
Unknown <rhalim@magicleap.com>
2017-12-28 19:20:03 -0500

    added assert method

--- src/test/java/providers/ElementAssertions.java
+++ src/test/java/providers/ElementAssertions.java
@@ -2,6 +2,7 @@ package providers;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
 public class ElementAssertions {
@@ -17,6 +18,27 @@ public class ElementAssertions {
         System.out.println("Asserting that element is exist for the locator '{}'" + locator);
         Boolean element = driver.findElements(locator).isEmpty();
         Assert.assertTrue(element,"The expected element does not exist");
+        return this;
+    }
+
+    public ElementAssertions elementDisplayed() {
+        System.out.println("Asserting that element is displayed for the locator '{}'" + locator);
+        Boolean SearchResults= driver.findElement(locator).isDisplayed();
+        Assert.assertTrue(SearchResults,"The expected element is not displayed");
+        return this;
+    }
+
+    public ElementAssertions elementEnabled() {
+        System.out.println("Asserting that element is enabled for the locator '{}'" + locator);
+        Boolean SearchResults= driver.findElement(locator).isEnabled();
+        Assert.assertTrue(SearchResults,"The expected element is not enabled");
+        return this;
+    }
+
+    public ElementAssertions elementSelected() {
+        System.out.println("Asserting that element is selected for the locator '{}'" + locator);
+        Boolean SearchResults= driver.findElement(locator).isSelected();
+        Assert.assertTrue(SearchResults,"The expected element is not selected");
         return this;
     }
 }
